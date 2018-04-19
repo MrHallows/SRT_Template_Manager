48f8dd7c59a3b6d66dd7b1a63ebba1923f82e390
MrHallows <autonomicstudios@outlook.com>
2018-04-19 01:09:40 -0700

    -

--- js/utils/settings.js
+++ js/utils/settings.js
@@ -8,14 +8,14 @@
 	<h3>General</h3>
 	<div class="controls">
 		<p>
-			<label class="checkboxCont">
+			<label class="checkbox-container">
 				<input type="checkbox" name="autoRun">
 				<span class="checkbox"></span>Auto-run code
 				<small>(beta)</small>
 			</label>
 		</p>
 		<p>
-			<label class="checkboxCont">
+			<label class="checkbox-container">
 				<input type="checkbox" name="tabIcons">
 				<span class="checkbox"></span>File Tree Tabs: Show icons instead of text
 				<small></small>
@@ -25,14 +25,14 @@
 	<h3>File Tree</h3>
 	<div class="controls">
 		<p>
-			<label class="checkboxCont">
+			<label class="checkbox-container">
 				<input type="checkbox" name="autoRun">
 				<span class="checkbox"></span>Auto-run code
 				<small>(beta)</small>
 			</label>
 		</p>
 		<p>
-			<label class="checkboxCont">
+			<label class="checkbox-container">
 				<input type="checkbox" name="tabIcons">
 				<span class="checkbox"></span>File Tree Tabs: Show icons instead of text
 				<small></small>
@@ -56,7 +56,7 @@ $('#rs-settings').on('click', function(e) {
 
     modal.open({
         title: 'Settings',
-        content: '<div id="app-settings"><h3>General</h3><div class="controls"><p><label class="checkboxCont"><input type="checkbox" name="autoRun"><span class="checkbox"></span>Auto-run code<small>(beta)</small></label></p><p><label class="checkboxCont"><input type="checkbox" name="tabIcons"><span class="checkbox"></span>File Tree Tabs: Show icons instead of text<small></small></label></p></div><h3>File Tree</h3><div class="controls"><p><label class="checkboxCont"><input type="checkbox" name="autoRun"><span class="checkbox"></span>Auto-run code<small>(beta)</small></label></p><p><label class="checkboxCont"><input type="checkbox" name="tabIcons"><span class="checkbox"></span>Tabs: Show icons instead of text<small></small></label></p></div></div><div class="actionbar"><button class="button btn-cancel">Cancel</button><button class="button btn-submit">Save</button></div>'
+        content: '<div id="app-settings"><h3>General</h3><div class="controls"><p><label class="checkbox-container"><input type="checkbox" name="autoRun"><span class="checkbox"></span>Auto-run code<small>(beta)</small></label></p><p><label class="checkbox-container"><input type="checkbox" name="tabIcons"><span class="checkbox"></span>File Tree Tabs: Show icons instead of text<small></small></label></p></div><h3>File Tree</h3><div class="controls"><p><label class="checkbox-container"><input type="checkbox" name="autoRun"><span class="checkbox"></span>Auto-run code<small>(beta)</small></label></p><p><label class="checkbox-container"><input type="checkbox" name="tabIcons"><span class="checkbox"></span>Tabs: Show icons instead of text<small></small></label></p></div></div><div class="actionbar"><button class="button btn-cancel">Cancel</button><button class="button btn-submit">Save</button></div>'
     });
 
     $('.button.btn-submit').on('click', function(e) {
